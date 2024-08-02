176eff8dc94597f60ebfb7c91b8f5eed962d65c1
talnz007 <Talhaniazai007@gmail.com>
2024-08-02 18:32:03 +0500

    Initial commit for the app

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
 import streamlit as st
-import os
-import git
 import pickle
 import requests
-import streamlit.components.v1 as components
 
-# Load token from Streamlit secrets
-token = st.secrets["github"]["token"]
-repo_url = f"https://{token}:x-oauth-basic@github.com/Talnz007/Movie_Recomendation_System.git"
-
-# Clone the repository
-repo_path = 'repo_path'
-if not os.path.exists(repo_path):
-    git.Repo.clone_from(repo_url, repo_path)
-else:
-    repo = git.Repo(repo_path)
-    repo.remotes.origin.pull()
-
-# Load data
-movies = pickle.load(open(os.path.join(repo_path, "movies_list.pkl"), 'rb'))
-similarity = pickle.load(open(os.path.join(repo_path, "similarity.pkl"), 'rb'))
-
-movie_list = movies['title'].values
-
-st.header("Movie Recommendation System")
 
 def fetch_poster(movie_id):
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US"
-    data = requests.get(url).json()
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(
+        movie_id)
+    data = requests.get(url)
+    data = data.json()
     poster_path = data['poster_path']
-    full_path = f"https://image.tmdb.org/t/p/w500/{poster_path}"
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
     return full_path
+
+
+movies = pickle.load(open("movies_list.pkl", 'rb'))
+similarity = pickle.load(open("similarity.pkl", 'rb'))
+movies_list = movies['title'].values
+
+st.header("Movie Recommender System")
+
+import streamlit.components.v1 as components
 
 imageCarouselComponent = components.declare_component("image-carousel-component", path="frontend/public")
 
@@ -48,12 +37,14 @@ imageUrls = [
     fetch_poster(914),
     fetch_poster(255709),
     fetch_poster(572154)
+
 ]
 
 imageCarouselComponent(imageUrls=imageUrls, height=200)
-selectvalue = st.selectbox("Pick movie from dropdown", movie_list)
+selectvalue = st.selectbox("Select movie from dropdown", movies_list)
 
-def recommendations(movie):
+
+def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector: vector[1])
     recommend_movie = []
@@ -64,23 +55,22 @@ def recommendations(movie):
         recommend_poster.append(fetch_poster(movies_id))
     return recommend_movie, recommend_poster
 
-if st.button("Show Recommendation"):
-    movie_name, recommend_poster = recommendations(selectvalue)
 
+if st.button("Show Recommend"):
+    movie_name, movie_poster = recommend(selectvalue)
     col1, col2, col3, col4, col5 = st.columns(5)
-
     with col1:
         st.text(movie_name[0])
-        st.image(recommend_poster[0])
+        st.image(movie_poster[0])
     with col2:
         st.text(movie_name[1])
-        st.image(recommend_poster[1])
+        st.image(movie_poster[1])
     with col3:
         st.text(movie_name[2])
-        st.image(recommend_poster[2])
+        st.image(movie_poster[2])
     with col4:
         st.text(movie_name[3])
-        st.image(recommend_poster[3])
+        st.image(movie_poster[3])
     with col5:
         st.text(movie_name[4])
-        st.image(recommend_poster[4])
+        st.image(movie_poster[4])
